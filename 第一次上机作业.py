c82d6240b106df43377ccae911b3dbd96df64f91
Citrus-Mei <2379700840@qq.com>
2022-09-14 14:58:38 +0800

    第一次上机作业提交

--- 第一次上机作业.py
+++ 第一次上机作业.py
@@ -1,17 +1,34 @@
 # 生成一个由100以内能够被5整除的数组成的列表，然后将其从大到小排序
-'''
+# '''
 values = []
 for value in range(5,101,5):
     values.append(str(value))
-    print(value)
 values.reverse()
 print(values)
-'''
+# '''
 
+# '''
+lst = [value for value in range(5,101,5)]
+print(lst)
+lsts = []
+for value in lst:
+    lsts.append(str(value))
+lsts.reverse()
+print(lsts)
+# '''
 
+# '''
+lst = list(range(5,101,5))
+print(lst)
+lsts = []
+for value in lst:
+    lsts.append(str(value))
+lsts.reverse()
+print(lsts)
+# '''
 
 # 定义一个列表，要求列表元素包含字符串、整数、浮点数然后把列表中不是字符串的元素全部删除
-'''
+# '''
 lists = ['xxx',100,100.1,100.2,'yyy',101,101.1,'aaa',101.2,'zzz',]
 print(lists)
 for list in lists[:]:
@@ -20,10 +37,10 @@ for list in lists[:]:
     else:
         lists.remove(list)
 print(lists)
-'''
+# '''
 
 # 输入英文姓名，按照字母顺序将所有姓名排序，输入完毕，将结果打印
-'''
+# '''
 name_lst = []
 while True:
     name = input("Please input an English name(input 'q' then exit)")
@@ -33,4 +50,4 @@ while True:
         name_lst.append(name)
 name_lst.sort()
 print(name_lst)
-'''
+# '''
