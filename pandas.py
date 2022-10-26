92763716b7ae3f665f11cde4f3dbcd509586e52c
吴锦颖 <2379700840@qq.com>
2022-10-26 03:17:34 +0000

    update pandas.py.
    Signed-off-by: 吴锦颖 <2379700840@qq.com>

--- pandas.py
+++ pandas.py
@@ -1,6 +1,8 @@
 from pandas import read_excel
 from pandas import DataFrame
 import pandas as pd
+
+'''
 pd.set_option('display.max_columns',10)
 df=read_excel('C:/Users/Administrator/Desktop/09电动1.xls')
 
@@ -40,3 +42,62 @@ df.drop('备注',axis=1)    # 设置删除轴向为1表示列
 df.columns['标题1','标题2']
 # 为数据添加一列备注
 # 为数据增加新的一行记录
+
+'''
+
+pd.set_option('display.max_columns',8)
+df = read_excel(r'D:data\rz2.xlsx')
+
+newDF = df.drop_duplicates()
+newDF = df.dropna()
+df.fillna(0)
+
+df,fillna(method = 'bfill')     # 后一个值填充缺失值
+
+df.IP.str.strip()
+df.IP.str.strip().str.slice(0,3)
+df.IP.str.strip('.')
+df.IP.str.strip('.', n = 2)
+df.IP.str.strip('.', n = 2, expand = True)
+
+df[df.TCSJ.str.contains('189', na = False)]
+df[df.TCSJ.astype(str).str.contains('189', na = False)]
+df[df.TCSJ.astype(str).str.contains('189', na = True)]
+
+import numpy as np
+df.loc[len(df)] = ['aa', np.nan, np.nan, np.nan, np,nan]
+df[df.IP.str.contains('222', na = False)]
+df[df.IP.str.contains('222', na = True)]
+
+fromn pandas import Series
+ss = Series([6,5,7,4])
+
+from pandas import DataFrame
+df0={'Ohio':[0,6,3],'Texas':[7,4,1],'California':[2,8,5]}
+df=DataFrame(df0.index=['a','c','d'])
+
+ser = Series([4.5,7.2,-5.3,3.6].index = ['a','b','c','d'])
+
+A = ['a','b','c','d','e']
+ser.reindex(A)
+
+A = ['d','b','c','e','a']
+ser.reindex(A,fill_value=0)
+
+ser = Series([4.5,7.2,-5.3,3.6],index = ['a','b','c','d'])
+A = ['d','b','c','e','a']
+ser.reindex(A, method='ffill')
+
+A = ['d','b','c','e','a']
+ser.reindex(A, method = 'bfill')
+
+A = ['d','b','c','e','a']
+ser.reindex(A, method = 'bfill',fill_value = 0)
+
+
+
+
+
+
+
+
